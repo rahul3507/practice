d90ffe0a04e6d6d3c7aa8787a83969cf7c9b2dc4
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-05-09 11:18:29 +0600

    added error handler

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,47 @@
 import { useRef } from 'react';
 import './App.css'
 import { useEffect } from 'react';
+import { useState } from 'react';
 
 function App() {
  
   const nameRef = useRef(null);
   const emailRef = useRef(null);
   const phoneRef = useRef(null);
-
+  const [error, setError] = useState('');
   useEffect(
     () => {
-      nameRef.current.focus();
-      nameRef.current.value = "Rahul";
-      
+      nameRef.current.focus();   
     }, []
   );
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(nameRef.current.value);
+
+    if (phoneRef.current.value.length < 11) {
+      setError('Phone number must be  11 digits');
+    }
+    else if (phoneRef.current.value.length === 11) {
+      setError('');
+      alert('Form submitted successfully');
+    }
 
   }
 
   return (
     <>
       <form action="" onSubmit={handleSubmit}>
-        <input ref={nameRef} type="text" name='name' />
+        <input ref={nameRef} defaultValue={'Rahul'} type="text" name='name' />
         <br />
-        <input ref={emailRef} type="email" name='email' />
+        <input ref={emailRef} type="email" name='email' required/>
         <br />
-        <input ref={phoneRef} type="text" name='phone' />
+        <input ref={phoneRef} type="text" name='phone' required/>
         <br />
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Submit"  />
         <br />
+        {error && <p style={{color: 'red'}}>{error}</p>}
       </form>
+      
         
     </>
   )
