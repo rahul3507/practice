95ac3ae826898d246fefc7df0e20203cf216d42f
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-05-09 10:33:20 +0600

    aded useRef into the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
 
+import { useRef } from 'react';
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
  
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const phoneRef = useRef(null);
+
+  useEffect(
+    () => {
+      nameRef.current.focus();
+      nameRef.current.value = "Rahul";
+      
+    }, []
+  );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(nameRef.current.value);
+
+  }
 
   return (
     <>
-      <form action="">
-        <input type="text" name='name' />
+      <form action="" onSubmit={handleSubmit}>
+        <input ref={nameRef} type="text" name='name' />
         <br />
-        <input type="email" value="email" id=''/>
+        <input ref={emailRef} type="email" name='email' />
         <br />
-        <input type="text" name='phone' />
+        <input ref={phoneRef} type="text" name='phone' />
         <br />
         <input type="submit" value="Submit" />
         <br />
