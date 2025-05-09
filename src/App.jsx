6d3b7b7bc8470612c7db225c00f0ad332b3e326a
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-05-09 10:17:15 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <form action="">
+        <input type="text" name='name' />
+        <br />
+        <input type="email" value="email" id=''/>
+        <br />
+        <input type="text" name='phone' />
+        <br />
+        <input type="submit" value="Submit" />
+        <br />
+      </form>
+        
+    </>
+  )
+}
+
+export default App
